f3599e1f476b1be4ce8e44895e3b4f7a0f6631f3
Scott Teal <scott@teals.org>
2024-02-06 11:20:34 -0500

    histogram fixes & tests

--- src/histogram.rs
+++ src/histogram.rs
@@ -3,11 +3,11 @@ use thiserror::Error;
 
 use crate::{
     bitstream::{BitStackWriter, BitStreamReader},
-    TABLE_LOG_MAX, TABLE_LOG_MIN, TABLE_LOG_RANGE
+    TABLE_LOG_MAX, TABLE_LOG_MIN, TABLE_LOG_RANGE,
 };
 
 /// A histogram of all the 8-bit symbols seen in a data sequence.
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Histogram {
     table: [u32; 256],
     size: u32,
@@ -65,6 +65,11 @@ impl Histogram {
         }
     }
 
+    /// Iterate over the populated part of the symbol frequency table
+    pub fn table_iter(&self) -> impl Iterator<Item = &u32> {
+        self.table.iter().take(self.max_symbol as usize + 1)
+    }
+
     /// Get the symbol frequency table
     pub fn table(&self) -> &[u32; 256] {
         &self.table
@@ -105,13 +110,7 @@ impl Histogram {
 
         let mut table = [0i32; 256];
 
-        for (i, (&t, t_norm)) in self
-            .table
-            .iter()
-            .zip(table.iter_mut())
-            .take(self.max_symbol as usize)
-            .enumerate()
-        {
+        for (i, (&t, t_norm)) in self.table_iter().zip(table.iter_mut()).enumerate() {
             if t == self.size {
                 *t_norm = to_distribute;
                 return NormHistogram {
@@ -131,19 +130,28 @@ impl Histogram {
             let mut prob = (t as u64 * step) >> scale;
             if prob < 8 {
                 let rest_to_beat = v_step * (RTB_TABLE[prob as usize] as u64);
-                prob += (t as u64 * step) - (((prob << scale) > rest_to_beat) as u64);
+                prob += ((t as u64 * step - (prob << scale)) > rest_to_beat) as u64;
             }
             let prob = prob as i32;
             if prob > largest_prob {
                 largest_prob = prob;
                 largest = i;
             }
+            if prob == 0 {
+                dbg!(&t);
+                dbg!(&prob);
+                dbg!(&step);
+                dbg!(&scale);
+                let init_prob = (t as u64 * step) >> scale;
+                dbg!(&init_prob);
+                panic!("We must not fill a probability of 0 when it's nonzero");
+            }
             *t_norm = prob;
             to_distribute -= prob;
         }
 
         // Check if we're in an unusual probability distribution
-        if -to_distribute >= (largest_prob >> 1) {
+        if to_distribute != 0 && -to_distribute >= (largest_prob >> 1) {
             return self.normalize_slow(log2);
         } else {
             table[largest] += to_distribute;
@@ -157,6 +165,7 @@ impl Histogram {
     }
 
     fn normalize_slow(self, log2: u32) -> NormHistogram {
+        println!("normalizing slowly");
         const UNASSIGNED: i32 = -2;
         let low_threshold = self.size >> log2;
         let low_one = (self.size * 3) >> (log2 + 1);
@@ -165,12 +174,7 @@ impl Histogram {
         let mut total = self.size;
 
         // Begin by distributing the low-probability symbols
-        for (&t, t_norm) in self
-            .table
-            .iter()
-            .zip(table.iter_mut())
-            .take(self.max_symbol as usize)
-        {
+        for (&t, t_norm) in self.table_iter().zip(table.iter_mut()) {
             if t == 0 {
                 continue;
             } else if t <= low_threshold {
@@ -201,7 +205,7 @@ impl Histogram {
                 .table
                 .iter()
                 .zip(table.iter_mut())
-                .take(self.max_symbol as usize)
+                .take(self.max_symbol as usize + 1)
             {
                 if *t_norm == UNASSIGNED && t <= low {
                     *t_norm = 1;
@@ -234,7 +238,7 @@ impl Histogram {
             // the remainder
             while to_distribute != 0 {
                 // Repeat until we're out of numbers to distribute.
-                for t in table.iter_mut().take(self.max_symbol as usize) {
+                for t in table.iter_mut().take(self.max_symbol as usize + 1) {
                     if *t > 0 {
                         *t += 1;
                         to_distribute -= 1;
@@ -245,18 +249,18 @@ impl Histogram {
                 }
             }
         } else {
-            let v_step_log = 62 - log2;
-            let mid = (1 << (v_step_log - 1)) - 1;
-            let r_step = (((1 << v_step_log) * to_distribute) + mid) / total;
+            let v_step_log: u64 = 62 - (log2 as u64);
+            let mid: u64 = (1 << (v_step_log - 1)) - 1;
+            let r_step: u64 = (((1 << v_step_log) * (to_distribute as u64)) + mid) / (total as u64);
             let mut tmp_total = mid;
             for (&t, t_norm) in self
                 .table
                 .iter()
                 .zip(table.iter_mut())
-                .take(self.max_symbol as usize)
+                .take(self.max_symbol as usize + 1)
             {
                 if *t_norm == UNASSIGNED {
-                    let end = tmp_total + (t * r_step);
+                    let end = tmp_total + (t as u64 * r_step);
                     let s_start = tmp_total >> v_step_log;
                     let s_end = end >> v_step_log;
                     let weight = s_end - s_start;
@@ -279,6 +283,7 @@ impl Histogram {
 
 /// A normalized histogram. Uses the special value "-1" to indicate a
 /// probability that was less than 1 in the original histogram.
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct NormHistogram {
     table: [i32; 256],
     log2: u32,
@@ -289,6 +294,11 @@ impl NormHistogram {
     // Get the normalized table
     pub fn table(&self) -> &[i32; 256] {
         &self.table
+    }
+
+    /// Iterate over the populated part of the symbol frequency table
+    pub fn table_iter(&self) -> impl Iterator<Item = &i32> {
+        self.table.iter().take(self.max_symbol as usize + 1)
     }
 
     // Get the base-2 logarithm of the sum of the table.
@@ -362,8 +372,10 @@ impl NormHistogram {
         let mut remaining = threshold + 1;
         let mut zero_count = 0;
         let mut num_bits = (self.log2 + 1) as usize;
-        for &s in self.table.iter().take(self.max_symbol() as usize + 1) {
-            if remaining <= 1 { break; }
+        for &s in self.table_iter() {
+            if remaining <= 1 {
+                break;
+            }
             if zero_count != 0 {
                 if s == 0 {
                     zero_count += 1;
@@ -407,8 +419,8 @@ impl NormHistogram {
     }
 
     /// Read in a normalized histogram that has been written out with [`write`][Self::write].
-    pub fn read(slice: &[u8]) -> Result<&[u8], HistError> {
-        let mut reader = BitStreamReader::new(slice, slice.len()*8);
+    pub fn read(slice: &[u8]) -> Result<(Self, &[u8]), HistError> {
+        let mut reader = BitStreamReader::new(slice, slice.len() * 8);
         let log2 = reader.read(4)? as u32 + TABLE_LOG_MIN;
         if log2 > TABLE_LOG_MAX {
             return Err(HistError::TableLogTooLarge(log2));
@@ -419,12 +431,12 @@ impl NormHistogram {
             max_symbol: 255,
         };
         let mut symbol = 0;
-        let mut threshold = 1<<hist.log2;
+        let mut threshold = 1 << hist.log2;
         let mut remaining = threshold + 1;
         let mut read_bit_count = log2 as usize + 1;
         let mut previous0 = false;
 
-        while remaining>1 && symbol < 256 {
+        while remaining > 1 && symbol < 256 {
             // Read in the 2-bit zero-continuation marks
             if previous0 {
                 while reader.peek(16).unwrap_or(0) == 0xFFFF {
@@ -437,19 +449,24 @@ impl NormHistogram {
                 }
                 symbol += reader.read(2)?;
             }
-            if symbol >= 256 { break; }
-
-            let max = (2*threshold-1) - remaining;
-            let raw_value = reader.peek(read_bit_count).or_else(|_| reader.peek(read_bit_count-1))?;
-            let mut value;
-            if (raw_value & (threshold-1)) < max {
-                reader.advance_by(read_bit_count-1)?;
-                value = raw_value & (threshold-1);
+            if symbol >= 256 {
+                break;
             }
-            else {
+
+            let max = (2 * threshold - 1) - remaining;
+            let raw_value = reader
+                .peek(read_bit_count)
+                .or_else(|_| reader.peek(read_bit_count - 1))?;
+            let mut value;
+            if (raw_value & (threshold - 1)) < max {
+                reader.advance_by(read_bit_count - 1)?;
+                value = raw_value & (threshold - 1);
+            } else {
                 reader.advance_by(read_bit_count)?;
-                value = raw_value & (2*threshold-1);
-                if value >= threshold { value -= max };
+                value = raw_value & (2 * threshold - 1);
+                if value >= threshold {
+                    value -= max
+                };
             }
             let value = value as i32 - 1;
             // Subtract out the retrieved count. Note: this will not ever
@@ -465,10 +482,12 @@ impl NormHistogram {
         }
 
         if remaining != 1 {
-            return Err(HistError::TooManySymbols)
+            return Err(HistError::TooManySymbols);
         }
 
-        Ok(reader.finish_byte())
+        hist.max_symbol = (symbol - 1) as u8;
+
+        Ok((hist, reader.finish_byte()))
     }
 }
 
@@ -480,4 +499,120 @@ pub enum HistError {
     TooManySymbols,
     #[error("Read error")]
     Io(#[from] std::io::Error),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::Fill;
+
+    fn hist_verify(hist: Histogram, log2: u32) {
+        print!("hist = [ ");
+        for &x in hist.table() {
+            print!("{}, ", x);
+        }
+        println!("]");
+        let hist_table = hist.table().to_owned();
+        let norm_hist = hist.normalize(log2);
+        print!("norm_hist = [ ");
+        for &x in norm_hist.table() {
+            print!("{}, ", x);
+        }
+        println!("]");
+        let sum: i32 = norm_hist.table.iter().map(|x| x.abs()).sum();
+        assert_eq!(sum, 1 << log2);
+        for (i, (h, hn)) in hist_table.iter().zip(norm_hist.table().iter()).enumerate() {
+            assert_eq!(
+                *h == 0,
+                *hn == 0,
+                "symbol {} histogram count was {} but normalized count is {}",
+                i,
+                h,
+                hn
+            );
+        }
+
+        let mut enc = Vec::with_capacity(norm_hist.write_bound());
+        norm_hist.write(&mut enc).unwrap();
+        let (dec_hist, rem) = NormHistogram::read(&enc).unwrap();
+        assert!(rem.len() <= 1);
+        assert_eq!(norm_hist, dec_hist);
+    }
+
+    #[test]
+    fn uniform_dist_256() {
+        // completely flat distribution
+        for log2 in 8..=TABLE_LOG_MAX {
+            let size = 1usize << log2;
+            let mut data = Vec::with_capacity(size);
+            for x in 0..=255u8 {
+                for _ in 0..(1 << (log2 - 8)) {
+                    data.push(x);
+                }
+            }
+            let hist = Histogram::new(&data);
+            for (j, &x) in hist.table().iter().enumerate() {
+                assert_eq!(
+                    x,
+                    1 << (log2 - 8),
+                    "symbol {} should be {} but is {}",
+                    j,
+                    1 << (log2 - 8),
+                    x
+                );
+            }
+            hist_verify(hist, log2);
+        }
+    }
+
+    #[test]
+    fn exp_dist() {
+        for log2 in 8..=TABLE_LOG_MAX {
+            let size = 1usize << log2;
+            let mut remaining = size;
+            let mut data = Vec::with_capacity(size);
+            let mut sym = 0;
+            loop {
+                for _ in 0..(remaining >> 1) {
+                    data.push(sym);
+                }
+                remaining -= remaining >> 1;
+                sym += 1;
+                if remaining == 1 {
+                    data.push(sym);
+                    break;
+                }
+            }
+            let hist = Histogram::new(&data);
+            for (j, &x) in hist.table().iter().enumerate() {
+                use std::cmp::Ordering;
+                let j = j as u32;
+                let expected = match j.cmp(&log2) {
+                    Ordering::Less => (1 << log2) >> (1 + j),
+                    Ordering::Equal => 1,
+                    Ordering::Greater => 0,
+                };
+                assert_eq!(
+                    x, expected,
+                    "symbol {} should be {} but is {}",
+                    j, expected, x
+                );
+            }
+            hist_verify(hist, log2);
+        }
+    }
+
+    #[test]
+    fn rand_dist_uniform() {
+        let mut rng = rand::thread_rng();
+        for log2 in 8..=TABLE_LOG_MAX {
+            let size = 1usize << (log2 + 2);
+            let mut data: Vec<u8> = vec![0u8; size];
+            for _ in 0..8 {
+                data.try_fill(&mut rng).unwrap();
+                let hist = Histogram::new(&data);
+                hist_verify(hist, log2);
+            }
+        }
+    }
 }
